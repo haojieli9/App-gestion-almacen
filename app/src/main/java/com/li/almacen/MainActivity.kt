581e4419e6943e2770d7a34eef49bd62f5fb6bae
ytian <1@g.com>
2024-02-05 21:09:45 +0100

    Toolbar

--- app/src/main/java/com/li/almacen/MainActivity.kt
+++ app/src/main/java/com/li/almacen/MainActivity.kt
@@ -2,10 +2,21 @@ package com.li.almacen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.li.almacen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
     }
 }
