2ac8d6fe9d96f1919c31063b93ca45e6f9bc86d4
haojieli <ty0192sd@gmail.com>
2024-05-18 10:48:13 +0200

    Formulario almacen

--- app/src/main/java/com/li/almacen/ui/fragments/fullscreendialog/AlmacenForm.kt
+++ app/src/main/java/com/li/almacen/ui/fragments/fullscreendialog/AlmacenForm.kt
@@ -1,14 +1,21 @@
 package com.li.almacen.ui.fragments.fullscreendialog
 
+import android.app.Dialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.res.ResourcesCompat.getDrawable
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.li.almacen.R
@@ -66,6 +73,13 @@ class ExampleDialog : DialogFragment() {
                 }
             }
         }
+
+        // Component validation
+        validateEditText(R.id.formTil1, R.id.formEdit1)
+        validateEditText(R.id.formTil2, R.id.formEdit2)
+        validateEditText(R.id.formTil3, R.id.formEdit3)
+        validateEditText(R.id.formTil4, R.id.formEdit4)
+        validateEditText(R.id.formTil5, R.id.formEdit5)
     }
 
     companion object {
@@ -78,11 +92,11 @@ class ExampleDialog : DialogFragment() {
     }
 
     private fun stockRegister() {
-        val nombre = binding.formEdit1.editText?.text.toString()
-        val descripcion = binding.formEdit3.editText?.text.toString()
-        val empleado = binding.formEdit5.editText?.text.toString()
-        val capacidad = binding.formEdit6.editText?.text.toString()
-        val ubicacion = binding.formEdit2.editText?.text.toString()
+        val nombre = binding.formEdit1.text.toString()
+        val descripcion = binding.formEdit2.text.toString()
+        val empleado = binding.formEdit3.text.toString()
+        val capacidad = binding.formEdit4.text.toString()
+        val ubicacion = binding.formEdit5.text.toString()
 
         val nuevoAlmacen = hashMapOf(
             "Nombre almacen" to nombre,
@@ -113,15 +127,46 @@ class ExampleDialog : DialogFragment() {
     }
 
     private fun confirmDialogBuilder() {
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle("GUARDAR ESTE ALMACÉN")
-            .setMessage("Deseas guardar este almacén?")
-            .setNeutralButton("Cancelar") { _, _ ->
-                Toast.makeText(requireContext(), "Cancelado", Toast.LENGTH_SHORT).show()
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.confirm_dialog)
+        dialog.window!!.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window!!.setBackgroundDrawable(getDrawable(resources, R.color.transparent, null))
+        dialog.setCancelable(false)
+        dialog.show()
+
+        val cancel = dialog.findViewById<Button>(R.id.testButton1)
+        val yes = dialog.findViewById<Button>(R.id.testButton2)
+
+        cancel.setOnClickListener {
+            Toast.makeText(requireContext(), "Operación cancelada.", Toast.LENGTH_LONG).show()
+
+        }
+
+        yes.setOnClickListener {
+            stockRegister()
+            Toast.makeText(requireContext(), "Almacen registrado correctamente.", Toast.LENGTH_LONG).show()
+            dialog.dismiss()
+            this@ExampleDialog.dismiss()
+        }
+    }
+
+    private fun validateEditText(layout: Int, edit: Int) {
+        val lay = requireView().findViewById<TextInputLayout>(layout)
+        val editt = requireView().findViewById<TextInputEditText>(edit)
+
+        editt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val text = s.toString()
+                when {
+                    text.isEmpty() -> lay.error = "Este campo es obligatorio."
+//                    text.length < 8 -> lay.error = "El nombre debe tener al menos 8 caracteres."
+                    else -> lay.error = null
+                }
             }
-            .setPositiveButton("Guardar") { _, _ ->
-                Toast.makeText(requireContext(), "Guardado", Toast.LENGTH_SHORT).show()
-            }
-            .show()
+        })
     }
 }
